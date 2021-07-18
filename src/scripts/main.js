c51c32bf60de0a98990af8b3f28ab7835bac8c2b
Hiran Karunananda <hiran.c@gmail.com>
2021-07-18 16:55:06 +0530

    styling

---
+++ src/scripts/main.js
@@ -0,0 +1,51 @@
+// object literal
+var myFeature = { 
+    filterSlider: function() {
+        
+        $('.creative-field-options').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });   
+    },
+    
+    init: function() {  
+        myFeature.filterSlider();
+
+    }
+
+}
+
+
+$(function () {    
+    //initialize core object
+    myFeature.init(); 
+
+});
